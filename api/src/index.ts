d90c09863f2bafb889547a7d19e283a56d307e53
sgrutman978 <sgrutman978@gmail.com>
2024-02-19 05:18:37 +0000

    login, new test, new loc, migration edits, untested

--- api/src/index.ts
+++ api/src/index.ts
@@ -24,39 +24,44 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 
+//TODO UPLOAD IMAGE METHOD
+
+
+app.post('/new-loc', async (req: Request, res: Response) => {
+  // TODO UPLOAD IMAGE AND SAVE FILE PATH TO PUSH HERE TO DB
+  try {
+    let name = req.body.name;
+    let image_path = req.body.image_path;
+    let lat = req.body.latitude;
+    let lng = req.body.longitude;
+    let epoch = req.body.epoch;
+
+    let sql = `INSERT INTO water_locs (name, image_path, latitude, longitude, epoch_added) VALUES '${name}', '${image_path}', ${lat}, ${lng}, ${epoch};`;
+    const res = await client.query(sql);
+
+    } catch (error) {
+      return res.status(500).json({ error: 'Error adding new location' });
+  }
+});
+
+
 app.post('/new-test', async (req: Request, res: Response) => {
   try {
-    let username = req.body.ph;
-    let lat = req.body.latitude;
-    let lng = req.body.longitude;
+    let ph = req.body.ph;
+    let tds = req.body.tds;
+    let temp = req.body.temp;
+    // ????? TODO ????? image path if we want to verify the person is actually at the water source via image taken and AI?
+    let locid = req.body.locid;
+    let userid = req.body.userid;
     let epoch = req.body.epoch;
     
-    let sql = " from users WHERE username='" + username + "';";
+    let sql = `INSERT INTO water_tests (user_id, loc_id, temperature, ph, tds, epoch) VALUES ${userid}, ${locid}, ${temp}, ${ph}, ${tds}, ${epoch};`;
     const res = await client.query(sql);
 
     } catch (error) {
       return res.status(500).json({ error: 'Error updating scores.' });
   }
 });
-
-
-/*app.post('/new-user', async (req: Request, res: Response) => {
-  try {
-    let username = req.body.ph;
-    let pub_key = req.body.tds;
-    let lat = req.body.latitude;
-    let lng = req.body.longitude;
-    let epoch = req.body.epoch;
-    let loginToken = req.body.loginToken;
-
-    let sql = " from users WHERE username='" + username + "';";
-    const res = await client.query(sql);
-
-    } catch (error) {
-      return res.status(500).json({ error: 'Error updating scores.' });
-  }
-});
-*/
 
 
 app.post('/signup', async (req: Request, res: Response) => {
@@ -73,10 +78,7 @@ app.post('/signup', async (req: Request, res: Response) => {
 
     const inserted = await sign_up(username, password, email, name, epoch_created).then(value => {return value;});
     if(inserted > 0){
-      const login_token = make_login_token(48);
-      let sql = `UPDATE users SET login_token='${login_token}' WHERE username='${username}';`;
-      const res2 = await client.query(sql);
-      return res.send({login_token: login_token});
+      return res.send({success: "success"});
     }else{
       return res.status(500).json({ error: 'Wrong sign up info, moron' });
     }
@@ -86,6 +88,29 @@ app.post('/signup', async (req: Request, res: Response) => {
   }
 });
 
+
+app.post('/login', async (req: Request, res: Response) => {
+  try {
+    const epoch = 0;//req.body.epoch_created;
+    //let x = encrypt.decryptLoginDataAES(epoch, msg);
+    //let json = JSON.parse(x);
+    const username = req.body.username; //json.username;
+    const password = req.body.password; //json.password;
+
+    const loginMatch = await login(username, password).then(value => {return value;});
+    if(loginMatch == 1){
+      const login_token = make_login_token(48);
+      let sql = `UPDATE users SET login_token='${login_token}', epoch=${epoch} WHERE username='${username}';`;
+      const res2 = await client.query(sql);
+      return res.send({login_token: login_token});
+    }else{
+      return res.status(500).json({ error: 'Wrong login info, moron' });
+    }
+  } catch (error) {
+          console.log(error);
+      return res.status(500).json({ error: 'Error signing up.' });
+  }
+});
 
 
 //app.use(express.static(__dirname + '/public'));
@@ -99,6 +124,19 @@ app.listen(port, () => {
 async function sign_up(username: string, password: string, email: string, name: string, epoch_created: number): Promise<number> {
     try {
     let sql = `INSERT INTO users (email, username, password, name, epoch_created, login_token) SELECT '${email}', '${username}', '${password}', '${name}', ${epoch_created}, '' WHERE NOT EXISTS (SELECT username FROM users WHERE username='${username}');`;
+    const res = await client.query(sql);
+    return res.rowCount || -1;
+    }catch (error) {
+      console.log(error);
+      return 0;
+  }
+}
+
+
+async function login(username: string, password: string, epoch_created: number): Promise<number> {
+  try {
+    // TODO ADD WHEN USER LOGGED IN WITH THE EPOCH    
+    let sql = `SELECT username from users WHERE username='${username}' AND password='${password}';`;
     const res = await client.query(sql);
     return res.rowCount || -1;
     }catch (error) {
